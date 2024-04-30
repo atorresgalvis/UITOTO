c88b827abbb3d52fe214c2789fc0f3fa41c5ac88
Ambrosio Torres Galvis <atorresgalvis@gmail.com>
2024-04-30 13:13:44 +0200

    Add files via upload

--- inst/app/tabs/ui/Search/UITOTObasic.R
+++ inst/app/tabs/ui/Search/UITOTObasic.R
@@ -5,26 +5,90 @@ Search <- tabPanel(title = "Find DMCs",
 	hr(),
 	sidebarLayout(
 		sidebarPanel(
-			fileInput("FastaFile", "Fasta file with the alignment:", accept = c(".fas", ".fasta", ".fs", ".FAS", ".FASTA", ".FS")),
-			fileInput("species", "File with the query-taxa:", accept = ".csv"),
-			checkboxInput("GapsNew", "Gaps are treated as a new state", FALSE),
+			fileInput("FastaFile",
+				label = tags$span(
+                  "Fasta file with the alignment", 
+                  tags$i(
+                    class = "glyphicon glyphicon-info-sign", 
+                    style = "color:#91bd0d;",
+                    title = "The name of the fasta file containing aligned sequences for obtaining the DMCs."
+                  ), ":"),			
+				accept = c(".fas", ".fasta", ".fs", ".FAS", ".FASTA", ".FS")
+			),
+			fileInput("species",  
+				label = tags$span(
+                  "File with the query-taxa", 
+                  tags$i(
+                    class = "glyphicon glyphicon-info-sign", 
+                    style = "color:#91bd0d;",
+                    title = "The name of the CSV file with the query-taxa list (i.e., taxa for which you want to find the DMCs)."
+                  ), ":"),
+				accept = ".csv"
+			),
+			checkboxInput("GapsNew",
+				label = tags$span(
+                  "Gaps are treated as a new state", 
+                  tags$i(
+                    class = "glyphicon glyphicon-info-sign", 
+                    style = "color:#91bd0d;",
+                    title = "Check this option if you want to treat the gaps as a new state."
+                )),
+				FALSE
+			),
 			tags$style("input[type=checkbox] { accent-color: #174364; }"),
 			tags$style("input[type=radio] { accent-color: #91bd0d; }"),
 			verbatimTextOutput("gaps"),
 			textInput("OutName", label = "Name of the output file:", value = "OpDMC_output.csv"),
-			sliderInput("iter", "Iterations x 1000 (i.e. 5 = 5000):",
+			sliderInput("iter", 
+				label = tags$span(
+                  "Iterations x 1000 (i.e. 5 = 5000)",
+                  tags$i(
+                    class = "glyphicon glyphicon-info-sign", 
+                    style = "color:#91bd0d;",
+                    title = "This is the number of molecular combinations that will be tested."
+                ), ":"),
 				min = 5, max = 150,
 				value = 20, step = 1
 			),
-			sliderInput("MnLen", "Minimum length:",
+			sliderInput("MnLen", 
+				label = tags$span(
+					"Minimum length",
+					tags$i(
+						class = "glyphicon glyphicon-info-sign", 
+						style = "color:#91bd0d;",
+						title = "This is the minimum length that the resulting DMCs must have."
+					), 
+					":"
+				),
 				min = 2, max = 15,
 				value = 4, step = 1
 			),	
-			sliderInput("exclusive", "Minimum number of exclusive character states:",
+			sliderInput("exclusive", 
+				label = tags$span(
+					"Minimum number of exclusive character states",
+					tags$i(
+						class = "glyphicon glyphicon-info-sign", 
+						style = "color:#91bd0d;",
+						title = "This is the minimum number of exclusive character states that the resulting DMCs must have.
+This implies that the resultant DMCs will possess, at minimum, the specified number of 
+exclusive character states when compared to other species."
+					), 
+					":"
+				),
 				min = 1, max = 10,
 				value = 4, step = 1
 			),	
-			sliderInput("RefStrength", "Refinement strength:",
+			sliderInput("RefStrength", 
+				label = tags$span(
+					"Refinement strength",
+					tags$i(
+						class = "glyphicon glyphicon-info-sign", 
+						style = "color:#91bd0d;",
+						title = "This is the proportion of sub-combinations from each resulting DMC that will be tested. 
+This option aims to identify potential shorter DMCs."
+					), 
+					":"
+				),
 				min = 0.0, max = 1,
 				value = 0.25, step = 0.05
 			),
